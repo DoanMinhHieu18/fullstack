a7d9d50978c4b7021150409994d219ab9e599e19
doanminhhieu <minhhieu180703@gmail.com>
2023-10-01 15:09:08 +0700

    first commit

---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Doan',
+      lastName: 'Minh Hieu',
+      address: 'Quang Nam',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
